6161c3d6ce7633d337291abc4842c84d44ccda3e
Diego Osvaldo <d.cruz@outlook.com>
2020-09-18 11:21:49 -0400

    Update foreingkey

--- mesi/settings.py
+++ mesi/settings.py
@@ -32,6 +32,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_userforeignkey.middleware.UserForeignKeyMiddleware',
 ]
 
 ROOT_URLCONF = 'mesi.urls'
